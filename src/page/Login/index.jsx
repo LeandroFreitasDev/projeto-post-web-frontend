2a75cbd8c151f2924f666b2847abf0ad0f7a05e0
unknown <leandrofreitas.dev@gmail.com>
2025-06-22 15:39:12 -0300

    pages

---
+++ src/page/Login/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../../components/Header'
+import Footer from '../../components/'
+
+export default function LoginPage() {
+  return (
+    <div>
+        <Header />
+        <h1>Login</h1>
+        <Footer />
+    </div>
+  )
+}
